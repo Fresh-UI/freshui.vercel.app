24fa202f39b4052fab65f190bf1573507cb84e4b
Debaditya Malakar <63513685+IronManCool001@users.noreply.github.com>
2021-01-22 12:54:42 +0530

    Add files via upload

--- public/pwabuilder-sw-register.js
+++ public/pwabuilder-sw-register.js
@@ -1,4 +1,4 @@
-// This is the "Background Sync" service worker
+// This is the "Offline page" service worker
 
 // Add this below content to your HTML page inside a <script type="module"></script> tag, or add the js file to your page at the very top to register service worker
 // If you get an error about not being able to import, double check that you have type="module" on your <script /> tag
